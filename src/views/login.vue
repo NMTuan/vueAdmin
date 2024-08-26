bf9f5f7beb8615a725098edf651da923c8cf9ce2
nmtuan <nmtuan@qq.com>
2024-08-26 20:37:22 +0800

    fix: 解决每次保存文件过慢的问题, 去掉了 eslint 相关内容

--- src/views/login.vue
+++ src/views/login.vue
@@ -9,57 +9,65 @@
  * Copyright (c) 2024 by nmtuan@qq.com, All Rights Reserved. 
 -->
 <template>
-  <div>
-    <el-form ref="formRef" :model="formData" :rules="rules" @submit.prevent="login">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="formData.username" placeholder="请输入用户名" />
-      </el-form-item>
+    <div>
+        <el-form
+            ref="formRef"
+            :model="formData"
+            :rules="rules"
+            @submit.prevent="login"
+        >
+            <el-form-item label="用户名" prop="username">
+                <el-input
+                    v-model="formData.username"
+                    placeholder="请输入用户名"
+                />
+            </el-form-item>
 
-      <el-form-item label="密码" prop="password">
-        <el-input
-          v-model="formData.password"
-          placeholder="请输入密码"
-          type="password"
-          @keyup.enter="login"
-        />
-      </el-form-item>
+            <el-form-item label="密码" prop="password">
+                <el-input
+                    v-model="formData.password"
+                    placeholder="请输入密码"
+                    type="password"
+                    @keyup.enter="login"
+                />
+            </el-form-item>
 
-      <el-form-item>
-        <el-button type="primary" @click="login">登录</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+            <el-form-item>
+                <el-button type="primary" @click="login">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script setup lang="ts">
-const formRef = ref(null)
+const formRef = ref(null);
 const formData = ref({
-  username: '111111',
-  password: '222222'
-})
+    username: "111111",
+    password: "222222",
+});
 
 const rules = ref({
-  username: [
-    { required: true, message: '请输入用户名' },
-    { min: 6, max: 18, message: '用户名长度在6到18个字符' }
-  ],
-  password: [
-    { required: true, message: '请输入密码' },
-    { min: 6, max: 18, message: '密码长度在6到18个字符' }
-  ]
-})
+    username: [
+        { required: true, message: "请输入用户名" },
+        { min: 6, max: 18, message: "用户名长度在6到18个字符" },
+    ],
+    password: [
+        { required: true, message: "请输入密码" },
+        { min: 6, max: 18, message: "密码长度在6到18个字符" },
+    ],
+});
 
 const login = async () => {
-  console.log('login', formData.value)
-  if (!formRef.value) {
-    return
-  }
-  formRef.value.validate(async (valid: boolean) => {
-    if (valid) {
-      await useUserStore().login(formData.value)
+    console.log("login", formData.value);
+    if (!formRef.value) {
+        return;
     }
-  })
-}
+    formRef.value.validate(async (valid: boolean) => {
+        if (valid) {
+            await useUserStore().login(formData.value);
+        }
+    });
+};
 </script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,49 @@
-import { fileURLToPath, URL } from 'node:url'
-import Unocss from 'unocss/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
+/*
+ * @Author: nmtuan nmtuan@qq.com
+ * @Date: 2024-08-26 19:52:34
+ * @LastEditors: nmtuan nmtuan@qq.com
+ * @LastEditTime: 2024-08-26 20:13:53
+ * @FilePath: \vueAdmin\vite.config.ts
+ * @Description:
+ *
+ * Copyright (c) 2024 by nmtuan@qq.com, All Rights Reserved.
+ */
+import { fileURLToPath, URL } from "node:url";
+import Unocss from "unocss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Unocss(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'pinia',
-        {
-          axios: [['default', 'axios']]
-        }
-        //  'element-plus'
-      ],
-      dirs: [
-        './src/stores',
-        './src/composables/**'
-      ],
-      dts: 'src/auto-imports.d.ts'
-    }),
-    Components({
-      // dts: 'src/components.d.ts',
-      // extensions: ['vue'],
-      // include: [/\.vue$/, /\.vue\?vue/],
-      // exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        vue(),
+        Unocss(),
+        AutoImport({
+            imports: [
+                "vue",
+                "vue-router",
+                "pinia",
+                {
+                    axios: [["default", "axios"]],
+                },
+                //  'element-plus'
+            ],
+            dirs: ["./src/stores", "./src/composables/**"],
+            dts: "src/auto-imports.d.ts",
+        }),
+        Components({
+            // dts: 'src/components.d.ts',
+            // extensions: ['vue'],
+            // include: [/\.vue$/, /\.vue\?vue/],
+            // exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/]
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
