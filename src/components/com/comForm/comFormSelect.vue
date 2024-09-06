078ae9c4e0a309fbb1e46b1d8566375113209dbe
nmtuan <nmtuan@qq.com>
2024-09-06 10:35:36 +0800

    chore: 整理 comForm 组件, 增加 date

---
+++ src/components/com/comForm/comFormSelect.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-select
+        v-model="formData[field.key]"
+        v-bind="field.props"
+        class="min-w-16"
+    >
+        <el-option
+            v-for="option in field.options"
+            :key="option.value"
+            :label="option.label"
+            :value="option.value"
+        >
+        </el-option>
+    </el-select>
+</template>
+<script setup>
+const field = inject('field', {})
+const formData = inject('formData', {})
+</script>
--- src/components/com/comForm/index.vue
+++ src/components/com/comForm/index.vue
@@ -2,45 +2,31 @@
  * @Author: nmtuan nmtuan@qq.com
  * @Date: 2024-08-30 10:52:14
  * @LastEditors: nmtuan nmtuan@qq.com
- * @LastEditTime: 2024-08-30 14:38:40
+ * @LastEditTime: 2024-09-06 06:16:17
  * @FilePath: \vueAdmin\src\components\com\comForm\index.vue
  * @Description: 
  * 
  * Copyright (c) 2024 by nmtuan@qq.com, All Rights Reserved. 
 -->
 <template>
-    <el-form :data="modelValue" v-bind="formProps">
+    <el-form :data="formData" v-bind="formProps">
         <el-form-item
             v-for="field in fields"
             :label="field.label"
             :key="field.key"
             :class="[formProps.labelPosition !== 'top' ? 'mb-0 !mr-3' : '']"
         >
-            <el-select
-                v-if="field.component === 'select'"
-                v-model="modelValue[field.key]"
-                v-bind="field.props"
-                class="min-w-16"
-            >
-                <el-option
-                    v-for="option in field.options"
-                    :key="option.value"
-                    :label="option.label"
-                    :value="option.value"
-                >
-                </el-option>
-            </el-select>
-            <el-input v-else v-model="modelValue[field.key]" />
+            <ComFormItem :field="field" />
         </el-form-item>
     </el-form>
 </template>
 <script setup>
 const props = defineProps({
     // 表单数据
-    modelValue: {
-        type: Object,
-        default: () => ({}),
-    },
+    // modelValue: {
+    //     type: Object,
+    //     default: () => ({}),
+    // },
     // 表单字段
     fields: {
         type: Array,
@@ -55,4 +41,7 @@ const props = defineProps({
         }),
     },
 });
+
+const formData = defineModel();
+provide("formData", formData);
 </script>
